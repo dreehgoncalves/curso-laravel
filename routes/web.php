0b23d1a63a907530e093677524df50e9a74de706
Andressa <andressagoncalves1997@gmail.com>
2021-12-31 09:45:49 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AddressController;
-use App\Http\Controllers\Form\TesteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,22 +14,6 @@ use App\Http\Controllers\Form\TesteController;
 |
 */
 
-Route::get('/', [PostController::class, 'showForm'])->name('showForm');
-
-Route::post('/debug', [PostController::class, 'debug'])->name('debug');
-
 Route::resource('usuarios', UserController::class)
     ->names('user')
     ->parameters(['usuarios' => 'user']);
-
-Route::resource('enderecos', AddressController::class)
-    ->names('address')
-    ->parameters(['enderecos' => 'address']);
-
-Route::resource('artigos', PostController::class)
-    ->names('post')
-    ->parameters(['artigos' => 'post']);
-
-// Route::resource('usuarios', TesteController::class)
-//     ->names('user')
-//     ->parameters(['usuarios' => 'user']);
